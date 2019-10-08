2def9e45b1c9ab623a8aa100582ae5a7e92595b8
charles <codelyfewithcharlie@gmail.com>
2019-10-08 10:40:20 -0700

    finised

--- src/App.js
+++ src/App.js
@@ -1,46 +1,57 @@
 import React, { Component } from "react";
 import "./App.css";
 
-class App extendz component {
-  contsructor(props) {
-    this.state = {
-      friendsList: ["john", "jacob", "jingle", "heimer", "schmidt"],
-      friend: ""
-    };
-  }
-  changeHandler(event) {
-    this.setState({
-      friend: event
-    });
-  }
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			friendsList: ["john", "jacob", "jingle", "heimer", "schmidt"],
+			friend: ""
+		};
+		this.deleteFriend = this.deleteFriend.bind(this);
+		this.addFriend = this.addFriend.bind(this);
+	}
+	changeHandler(event) {
+		this.setState({
+			friend: event
+		});
+	}
 
-  addFriend() {
-    this.state.friendsList.push(this.state.friend);
-  }
+	addFriend() {
+		const friendListCopy = [...this.state.friendsList];
+		friendListCopy.push(this.state.friend);
+		this.setState({ friendsList: friendListCopy, friend: "" });
+	}
 
-  deleteFriend(index) {
-    this.state.friendsList.splice(index, 1);
-  }
+	deleteFriend(index) {
+		const friendListCopy = [...this.state.friendsList];
+		friendListCopy.splice(index, 1);
+		this.setState({ friendsList: friendListCopy });
+	}
 
-  render() {
-    const mappedFreinds = this.state.friendsList.map((friend, index) => {
-      <div>
-        <span key={index}>{friend}</span>
-        <button onClick={this.state.deleteFriend} />
-      </div>;
-    });
-    return (
-      <div className="App">
-        <div>{mappedFreinds}</div>
-        <input
-          type="number"
-          onChange={this.changeHandler}
-          value={this.state.friend}
-        />
-        <button onClick={this.addFriend}>Add Friend</button>
-      </div>
-    );
-  }
+	render() {
+		const mappedFreinds = this.state.friendsList.map((friend, index) => {
+			return (
+				<div key={index}>
+					<span>{friend}</span>
+					<button onClick={this.deleteFriend}>Delete</button>
+				</div>
+			);
+		});
+		return (
+			<div className='App'>
+				<div>{mappedFreinds}</div>
+				<input
+					type='text'
+					onChange={e => {
+						this.changeHandler(e.target.value);
+					}}
+					value={this.state.friend}
+				/>
+				<button onClick={this.addFriend}>Add Friend</button>
+			</div>
+		);
+	}
 }
-}
+
 export default App;
